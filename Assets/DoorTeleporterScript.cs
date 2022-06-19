53e910b3d0db1d1e826a39f7309f1d1a000e3ca5
Comrade-Sev <79182100+Comrade-Sev@users.noreply.github.com>
2022-06-19 03:54:58 +0200

    Teleport done and camera movement too

--- Assets/DoorTeleporterScript.cs
+++ Assets/DoorTeleporterScript.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class DoorTeleporterScript : MonoBehaviour
 {
 
     public Transform player;
     public Transform reciever;
+    public NavMeshAgent PlayerAgent;
+    public Vector3 dest = new Vector3();
+    public Vector3 currentPosition;
+    public bool checker = false;
+
 
     private bool playerIsOverlapping = false;
     void Update()
     {
-        Debug.Log(playerIsOverlapping);
+        Debug.Log(checker);
+        /*Debug.Log(playerIsOverlapping);
         if (playerIsOverlapping)
         {
             Vector3 portalToPlayer = player.position - transform.position;
@@ -22,20 +29,27 @@ public class DoorTeleporterScript : MonoBehaviour
             {
                 //Teleport the palyer
                 float rotationDiff = -Quaternion.Angle(transform.rotation, reciever.rotation);
-                rotationDiff += 180;
                 player.Rotate(Vector3.up, rotationDiff);
-
                 Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
                 player.position = reciever.position + positionOffset;
 
                 playerIsOverlapping = false;
             }
+        }*/
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            PlayerAgent.Warp(dest);
+            currentPosition = GameObject.Find("Player").GetComponent<NewPlayerMovement>().hitInfo.point;
+            checker = true;
         }
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "player")
+        if (other.tag == "Player")
         {
             playerIsOverlapping = true;
         }
@@ -43,7 +57,7 @@ public class DoorTeleporterScript : MonoBehaviour
 
     void OnTriggerExit(Collider other)
     {
-        if (other.tag == "player")
+        if (other.tag == "Player")
         {
             playerIsOverlapping = false;
         }
